6bac03f4b9f4a0f42e6c12da3e248de620295d7b
Simon Prickett <simon@redislabs.com>
2022-03-14 20:32:10 +0000

    Changed the way I render the page.

--- app.py
+++ app.py
@@ -5,20 +5,23 @@ app = Flask(__name__)
 
 r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
+def render_page():
+    count = r.get("mycounter")
+    if count is None:
+        count = 0
+
+    return f'<h1>Flask Redis Starter Application</h1><p>Count:{count}</p><ul><li><a href="/incr">Increment counter</a></li><li><a href="/reset">Reset counter</a></li></ul>'
+
 @app.route("/incr")
 def incr():
     count = r.incr("mycounter", 1)
-    return redirect("/")
+    return render_page()
 
 @app.route("/reset")
 def reset():
     r.delete("mycounter")
-    return redirect("/")
+    return render_page()
 
 @app.route("/")
 def home():
-    count = r.get("mycounter")
-    if count is None:
-        count = 0
-        
-    return f'<h1>Flask Redis Starter Application</h1><p>Count:{count}</p><ul><li><a href="/incr">Increment counter</a></li><li><a href="/reset">Reset counter</a></li></ul>'
+    return render_page()
